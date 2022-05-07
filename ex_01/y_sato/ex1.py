bbc1afd041009cf437f6eecdee05145af684c632
Yuuki310 <sato.yuki@g.sp.m.is.nagoya-u.ac.jp>
2022-05-07 14:02:56 +0900

    修正版

--- ex_01/y_sato/ex1.py
+++ ex_01/y_sato/ex1.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
     #Original Signal
     ax1 = fig.add_subplot(3,1,1)
     ax1.plot(time,original_signal)
-    ax1.set_xlabel("Time(s)")
+    ax1.set_xlabel("Time[s]")
     ax1.set_ylabel("Sound Amplitude")
     ax1.set_title("Original Signal")
 
@@ -57,23 +57,23 @@ if __name__ == "__main__":
     ax2 = fig.add_subplot(3,1,2)
 
     spec_log = 20 * np.log10(np.abs(spec).T)[window // 2:] #dB
-    im = ax2.imshow(spec_log,extent = [0,original_signal.shape[0] // sr,0,sr // 2,] ,aspect="auto")
+    im = ax2.imshow(spec_log,cmap='jet',extent = [0,original_signal.shape[0] // sr,0,sr // 2,] ,aspect="auto")
 
     #ax2.set_yscale("log",base=2)
     #ax2.set_ylim([50,sr//2])
-    ax2.set_xlabel('Time [s]')
-    ax2.set_ylabel('Frequency [Hz]')
+    ax2.set_xlabel('Time[s]')
+    ax2.set_ylabel('Frequency[Hz]')
 
     divider = make_axes_locatable(ax2)
     cax = divider.append_axes('right', '2%', pad=0.1)
     cbar = fig.colorbar(im, format='%+2.0f dB', cax=cax)
-    cbar.set_label("Magnitude [dB]")
+    cbar.set_label("Magnitude[dB]")
     ax2.set_title("Spectrogram")
 
     #Re-Synthesized Signal
     ax3 = fig.add_subplot(3,1,3)
     ax3.plot(time,ispec)
-    ax3.set_xlabel("Time(s)")
+    ax3.set_xlabel("Time[s]")
     ax3.set_ylabel("Sound Amplitude")
     ax3.set_title("Re-Synthesized Signal")
 
