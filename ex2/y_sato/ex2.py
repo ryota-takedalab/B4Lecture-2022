c055a0cb253e81ee6867c0ea7213eb5946f5f946
Yuuki310 <sato.yuki@g.sp.m.is.nagoya-u.ac.jp>
2022-05-15 16:30:02 +0900

    all filter

--- ex2/y_sato/ex2.py
+++ ex2/y_sato/ex2.py
@@ -29,7 +29,6 @@ def HPF(fq,sr,fir_size = 512):
     pi = np.pi
     omega = 2 * pi * fq / sr
     arange = np.arange(-fir_size//2,fir_size//2)
-    print(arange)
     filter = np.sinc(arange) - omega * np.sinc(omega*arange / pi)/pi
     window = np.hamming(fir_size)
     return filter * window
@@ -44,6 +43,26 @@ def LPF(fq,sr,fir_size = 512):
     return filter * window
 
 
+def BPF(low,high,sr,fir_size = 512):
+    pi = np.pi
+    low_omega = 2 * pi * low / sr
+    high_omega = 2 * pi * high / sr
+    arange = np.arange(-fir_size//2,fir_size//2)
+    filter = (high_omega * np.sinc(high_omega*arange / pi) - low_omega * np.sinc(low_omega*arange / pi))/pi
+    window = np.hamming(fir_size)
+    return filter * window
+
+
+def BEF(low,high,sr,fir_size = 512):
+    pi = np.pi
+    low_omega = 2 * pi * low / sr
+    high_omega = 2 * pi * high / sr
+    arange = np.arange(-fir_size//2,fir_size//2)
+    filter = np.sinc(arange) - (high_omega * np.sinc(high_omega*arange / pi) + low_omega * np.sinc(low_omega*arange / pi))/pi
+    window = np.hamming(fir_size)
+    return filter * window
+
+
 def convolution(input,filter):
     input_len = len(input)
     filter_len = len(filter)
@@ -53,6 +72,21 @@ def convolution(input,filter):
         result[i : i+filter_len] += np.multiply(input[i] , filter)
 
     return result
+
+
+def Spectrogram(ax,spec,frame_length):
+    spec_log = 20 * np.log10(np.abs(spec).T)[window // 2:] #dB
+    im = ax.imshow(spec_log,cmap='jet',extent = [0,frame_length // sr,0,sr // 2,] ,aspect="auto")
+    ax.set_yscale("log",base=2)
+    ax.get_yaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
+    ax.set_ylim([50,20000])
+    ax.set_xlabel('Time[s]')
+    ax.set_ylabel('Frequency[Hz]')
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes('right', '2%', pad=0.1)
+    cbar = fig.colorbar(im, format='%+2.0f dB', cax=cax)
+    cbar.set_label("Magnitude[dB]")
+    ax.set_title("Spectrogram")
 
 
 if __name__ == "__main__":
@@ -71,7 +105,7 @@ if __name__ == "__main__":
     #STFT
     original_spec = STFT(original_signal,window,step)
 
-    filter =HPF(1000,sr=sr)
+    filter =HPF(500,sr=sr)
     filter_p = np.fft.fft(filter)
     filtered_signal = convolution(original_signal,filter) #時間領域
     filtered_spec = STFT(filtered_signal,window,step) #周波数領域に変換
@@ -86,20 +120,9 @@ if __name__ == "__main__":
     ax1.set_ylabel("Sound Amplitude")
     ax1.set_title("Original Signal")
 
-    #Originak Spectrogram
+    #Original Spectrogram
     ax2 = fig.add_subplot(3,2,2)
-    spec_log = 20 * np.log10(np.abs(original_spec).T)[window // 2:] #dB
-    im = ax2.imshow(spec_log,cmap='jet',extent = [0,original_signal.shape[0] // sr,0,sr // 2,] ,aspect="auto")
-    ax2.set_yscale("log",base=2)
-    ax2.get_yaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
-    ax2.set_ylim([50,20000])
-    ax2.set_xlabel('Time[s]')
-    ax2.set_ylabel('Frequency[Hz]')
-    divider = make_axes_locatable(ax2)
-    cax = divider.append_axes('right', '2%', pad=0.1)
-    cbar = fig.colorbar(im, format='%+2.0f dB', cax=cax)
-    cbar.set_label("Magnitude[dB]")
-    ax2.set_title("Spectrogram")
+    Spectrogram(ax2,original_spec,frame_length)
 
     #Filter property
     ax3 = fig.add_subplot(3,2,3)
@@ -113,18 +136,11 @@ if __name__ == "__main__":
 
     #Filtered Signal Spectrogram
     ax4 = fig.add_subplot(3,2,4)
-    spec_log = 20 * np.log10(np.abs(filtered_spec).T)[window // 2:] #dB
-    im = ax4.imshow(spec_log,cmap='jet',extent = [0,original_signal.shape[0] // sr,0,sr // 2,] ,aspect="auto")
-    ax4.set_yscale("log",base=2)
-    ax4.get_yaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
-    ax4.set_ylim([50,20000])
-    ax4.set_xlabel('Time[s]')
-    ax4.set_ylabel('Frequency[Hz]')
-    ax4.set_title("Filtered Signal")
-    divider = make_axes_locatable(ax4)
-    cax = divider.append_axes('right', '2%', pad=0.1)
-    cbar = fig.colorbar(im, format='%+2.0f dB', cax=cax)
-    cbar.set_label("Magnitude[dB]")
+    Spectrogram(ax4,filtered_spec,frame_length)
+
+        #ISTFT
+    ispec = ISTFT(filtered_spec,frame_length,window,step)
+    sf.write("resynthesized.wav", ispec, sr, subtype="PCM_16")
 
     plt.tight_layout()
     plt.show()
