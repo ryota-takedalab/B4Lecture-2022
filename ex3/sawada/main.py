934597e97ac4d2998614fa91fb58c4a611fb7cc0
KateSawada <sawapy630@gmail.com>
2022-05-20 16:39:58 +0900

    implemented 3d regression

--- ex3/sawada/main.py
+++ ex3/sawada/main.py
@@ -9,8 +9,9 @@ from mpl_toolkits.mplot3d import Axes3D
 def regression_2d(data, title, dim=1,
                   regularize=False, reg_coef=1.0):
     data_regression = np.zeros((data.shape[0], dim + 1))
-    x_linspace = np.linspace(np.min(data[:, 0]), np.max(data[:, 0]), 50)
-    x_plot = np.zeros((50, dim))
+    density = 50
+    x_linspace = np.linspace(np.min(data[:, 0]), np.max(data[:, 0]), density)
+    x_plot = np.zeros((density, dim))
     
     for i in range(dim):
         data_regression[:, i] = np.power(data[:, 0], i + 1)
@@ -22,26 +23,74 @@ def regression_2d(data, title, dim=1,
     fig, ax = plt.subplots(figsize=(12, 10))
     fig.subplots_adjust(hspace=0.6)
 
-    if len(data[0] == 1):
-        # single regression
-        ax.set_title(title)
-        ax.set_xlabel("x1")
-        ax.set_ylabel("y")
-        ax.scatter(data[:, 0], data[:, 1], color="blue",
-                   label="Observed data")
+    ax.set_title(title)
+    ax.set_xlabel("x1")
+    ax.set_ylabel("y")
+    ax.scatter(data[:, 0], data[:, 1], color="blue",
+               label="Observed data")
 
-        # regression result
-        y_reg = np.concatenate([np.ones((len(x_plot), 1)), x_plot], 1) \
-            @ coefficient
+    # regression result
+    y_reg = np.concatenate([np.ones((len(x_plot), 1)), x_plot], 1) \
+        @ coefficient
 
-        label = "y = " + "{:.3g}".format(coefficient[0])
-        for i in range(len(coefficient) - 1):
-            label += " + " + "{:.3g}".format(coefficient[i + 1]) \
-                + f"$x^{i+1}$"
-        ax.plot(x_plot[:, 0], y_reg, color="red", label=label)
-        ax.legend()
-        plt.savefig(f"{title}_{dim}th_polynomial.png")
-        plt.show()
+    label = "y = " + "{:.3g}".format(coefficient[0])
+    for i in range(len(coefficient) - 1):
+        label += " + " + "{:.3g}".format(coefficient[i + 1]) \
+            + f"$x^{i+1}$"
+    ax.plot(x_plot[:, 0], y_reg, color="red", label=label)
+    ax.legend()
+    plt.savefig(f"{title}_{dim}th_polynomial.png")
+    plt.show()
+        
+        
+def regression_3d(data, title, dim=1,
+                  regularize=False, reg_coef=1.0):
+    density = 50
+    data_regression = np.zeros(
+        (data.shape[0], ((dim + 1) * (dim + 2)) // 2))
+    x1_linspace = np.linspace(np.min(data[:, 0]), np.max(data[:, 0]), density)
+    x2_linspace = np.linspace(np.min(data[:, 1]), np.max(data[:, 1]), density)
+    x1, x2 = np.meshgrid(x1_linspace, x2_linspace)
+    x_plot = np.zeros((density, density, ((dim + 1) * (dim + 2)) // 2 - 1))
+    
+    col = 0
+    for i in range(dim):
+        for j in range(i + 2):
+            data_regression[:, col] \
+                = np.power(data[:, 0], (i + 1 - j)) \
+                * np.power(data[:, 1], (j))
+            x_plot[:, :, col] \
+                = np.power(x1, (i + 1 - j)) \
+                * np.power(x2, (j))
+            col += 1
+    data_regression[:, -1] = data[:, -1]
+
+    coefficient = linear_regression(data_regression, regularize, reg_coef)
+    # plot initialization
+    fig, ax = plt.subplots(figsize=(12, 10), subplot_kw=dict(projection='3d'))
+    ax = Axes3D(fig)
+    fig.subplots_adjust(hspace=0.6)
+
+    ax.set_title(title)
+    ax.set_xlabel('$x_1$')
+    ax.set_ylabel('$x_2$')
+    ax.set_zlabel('y')
+    ax.scatter(data.T[0], data.T[1], data.T[2],
+               color="blue", label="Ovserved data")
+    # regression result
+    y_reg = np.concatenate([np.ones((density, density, 1)), x_plot], 2) \
+        @ coefficient
+    label = "y = " + "{:.3g}".format(coefficient[0])
+    col = 1
+    for i in range(dim):
+        for j in range(i + 2):
+            label += " + " + "{:.3g}".format(coefficient[col]) \
+                + f"$x_1^{i + 1 - j}x_2^{j}$"
+            col += 1
+    ax.plot_wireframe(x1, x2, y_reg, color="red", label=label)
+    ax.legend()
+    plt.savefig(f"{title}_{dim}th_polynomial.png")
+    plt.show()
 
 
 def linear_regression(data, regularize=False, reg_coef=1.0):
@@ -86,4 +135,7 @@ if __name__ == "__main__":
 
     if (data.shape[1] == 2):
         regression_2d(data, f"data{args.input}", dim=args.dimension,
+                      regularize=args.regularize)
+    if (data.shape[1] == 3):
+        regression_3d(data, f"data{args.input}", dim=args.dimension,
                       regularize=args.regularize)
