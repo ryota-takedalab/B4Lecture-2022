5cb1e2130ed15b9d99cf3b21a9302b16c1edbb70
tahayakawa <hayakawa.tatsuya@g.sp.m.is.nagoya-u.ac.jp>
2022-05-24 00:33:48 +0900

    gif追加

--- ex3/t_hayakawa/main.py
+++ ex3/t_hayakawa/main.py
@@ -1,6 +1,8 @@
 import numpy as np
 import argparse
 import matplotlib.pyplot as plt
+from PIL import Image
+from io import BytesIO
 
 
 def regression_2d(x, y, dimension, lamda):
@@ -22,7 +24,7 @@ def regression_2d(x, y, dimension, lamda):
     _x = np.linspace(x.min(), x.max(), 100)
     _y = np.array([np.dot(a, np.array([_x**n for n in range(dimension+1)]))])
 
-    #equation name
+    # equation name
     name = ""
     for i in range(dimension+1):
         if i > 0:
@@ -57,7 +59,7 @@ def regression_3d(x0, x1, y, dimension, lamda):
     a = np.dot(np.dot(np.linalg.pinv(
         np.dot(xx.T, xx)+lamda*iden_mat), xx.T), y)
 
-    #equation name
+    # equation name
     name = ""
     for i in range(dimension+1):
         if i > 0:
@@ -78,6 +80,25 @@ def regression_3d(x0, x1, y, dimension, lamda):
     return a, name
 
 
+def render_frame(x0, x1, y, _x0, _x1, _y, label_name, title_name, angle):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    ax.plot_wireframe(_x0, _x1, _y, linewidth=0.2,
+                      color="red", label=label_name)
+    ax.scatter(x0, x1, y, label="Observed data")
+    ax.view_init(elev=30, azim=angle)
+    plt.close()
+    ax.set_title(title_name, fontsize=20)
+    ax.set_xlabel("$x_0$")
+    ax.set_ylabel("$x_1$")
+    ax.set_zlabel("$y$")
+    ax.legend(bbox_to_anchor=(1, 1))
+
+    buf = BytesIO()
+    fig.savefig(buf, bbox_inches="tight", pad_inches=0.0)
+    return Image.open(buf)
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("filename", type=str, help="input filename .csv")
@@ -86,21 +107,21 @@ def main():
 
     args = parser.parse_args()
 
-    #load text from .csv
+    # load text from .csv
     data = np.loadtxt(args.filename+".csv", delimiter=",", skiprows=1)
 
-    #split data
+    # split data
     x = data[:, :-1]
     y = data[:, -1]
 
     if x.shape[1] == 1:
         _x, _y, name = regression_2d(x[:, 0], y, args.dim, args.lamda)
 
-        #plot
+        # plot
         fig = plt.figure(figsize=(8, 6))
         plt.plot(_x, _y, c="r", label=name)
         plt.scatter(x[:, 0], y, label="Observed data")
-        plt.title(args.filename)
+        plt.title(args.filename, fontsize=20)
         plt.xlabel("$x_0$")
         plt.ylabel("$y$")
         plt.legend()
@@ -109,7 +130,7 @@ def main():
     if x.shape[1] == 2:
         a, name = regression_3d(x[:, 0], x[:, 1], y, args.dim, args.lamda)
 
-        #calcurate "y" using weight
+        # calcurate "y" using weight
         x0 = np.linspace(x[:, 0].min(), x[:, 0].max(), 100)
         x1 = np.linspace(x[:, 1].min(), x[:, 1].max(), 100)
         _x0, _x1 = np.meshgrid(x0, x1)
@@ -117,17 +138,22 @@ def main():
         for i in range(args.dim+1):
             _y += a[i]*(_x0**i)+a[i+args.dim+1]*(_x1**i)
 
-        #plot
-        fig = plt.figure(figsize=(10, 8))
+        # gif plot
+        images=[render_frame(x[:, 0], x[:, 1], y, _x0, _x1, _y, name, args.filename,angle) for angle in range(180)]
+        images[0].save("data3_reg.gif",save_all=True, append_images=images[1:], duration=100, loop=0)
+
+        # plot
+        """ fig = plt.figure(figsize=(8, 6))
         ax1 = fig.add_subplot(121, projection="3d")
         ax1.plot_wireframe(_x0, _x1, _y, linewidth=0.2,
                            color="red", label=name)
         ax1.scatter(x[:, 0], x[:, 1], y, label="Observed data")
+        ax1.view_init(elev=30, azim=-45)
         ax1.set_title(args.filename)
         ax1.set_xlabel("$x_0$")
         ax1.set_ylabel("$x_1$")
         ax1.set_zlabel("$y$")
-        ax1.legend(bbox_to_anchor=(1, 1.03))
+        ax1.legend(bbox_to_anchor=(1, 1.03)) 
 
         ax2 = fig.add_subplot(122, projection="3d")
         ax2.plot_wireframe(_x0, _x1, _y, linewidth=0.2, color="red")
@@ -136,10 +162,10 @@ def main():
         ax2.set_title(args.filename)
         ax2.set_xlabel("$x_0$")
         ax2.set_ylabel("$x_1$")
-        ax2.set_zlabel("$y$")
+        ax2.set_zlabel("$y$") 
 
         plt.tight_layout()
-        plt.show()
+        plt.show() """
 
 
 if __name__ == "__main__":
