8d565beea087bbf6d02791059d8881b33f3e1026
imsatomi <t.sato.sakc.3103@gmail.com>
2022-05-25 00:19:52 +0900

    グラフ名作成方法修正,docstring追加,他微修正

--- ex3/s_tokida/ex3.py
+++ ex3/s_tokida/ex3.py
@@ -5,7 +5,22 @@ import csv
 import argparse
 from mpl_toolkits.mplot3d import Axes3D
 
-def least_squares(x1,x2,dim):
+def least_squares_2d(x1, x2, dim,l):
+    """least squares regression
+    
+    Parameters:
+    ----------
+    x1 (ndarray) : input x1 data
+    x2 (ndarray) : input x2 data
+    dim (int) : dimention
+    l (float) : regularization coefficient 
+    
+    Returns:
+    ----------
+    predect_y (def) : define y and linename
+    w (ndarray) : weight
+
+    """
     # Tの作成
     T = np.array([(x1**j*x2).sum() for j in range(dim+1)])
 
@@ -16,21 +31,42 @@ def least_squares(x1,x2,dim):
         A = pd.concat([A, pd.DataFrame([a])])
 
     # Aの逆行列を計算
-    Ainv = np.linalg.inv(A)
+    I = np.eye(dim+1)
+    # Ainv = np.linalg.inv(A)  # 正則化なし
+    Ainv = np.linalg.inv(A + l+I)  # 正則化あり
 
     # 係数の解 w = Ainv * T
-    w = np.dot(Ainv, T)
+    w = np.dot(Ainv, T)  # [1.08898963 2.0207883 ]
 
     # y座標の計算
     def predict_y(x):
         y=0
+        linename = ''
         for i, w_i in enumerate(w):
             y+=w_i * (x**i)
-        return y
+            linename += f'{w[i]:.2f}x^{i}+'
+        linename = linename[:-1]
+        return y, linename
 
-    return(predict_y, w)
+    return predict_y, w
 
-def least_squares_3(x1, x2, x3, dim, lamda):
+def least_squares_3d(x1, x2, x3, dim, l):
+    """least squares regression
+    
+    Parameters:
+    ----------
+    x1 (ndarray) : input x1 data
+    x2 (ndarray) : input x2 data
+    x3 (ndarray) : input x3 data
+    dim (int) : dimention
+    l (float) : regularization coefficient 
+    
+    Returns:
+    ----------
+    predect_y (def) : define z and linename
+    w (ndarray) : weight
+
+    """
     # phiの作成
     n = len(x1)  # 100
     phi = np.zeros([n, 1 + dim*2])
@@ -40,16 +76,21 @@ def least_squares_3(x1, x2, x3, dim, lamda):
     # Iの作成
     I = np.eye(1 + dim*2)
     # w = np.dot(np.dot(np.linalg.pinv(np.dot(phi.T,phi)), phi.T), x3)  # 正則化なし
-    w = np.dot(np.dot(np.linalg.pinv(np.dot(phi.T,phi)+lamda*I), phi.T), x3)  # 正則化あり
+    w = np.dot(np.dot(np.linalg.inv(np.dot(phi.T,phi)+l*I), phi.T), x3)  # 正則化あり # [[-48.57517547][  2.12646582][ 29.59299973]]
+    w = [x for row in w for x in row]  # wの１次元化
    
     # z座標の計算
     def predict_z(x, y):
         z = 0
+        linename = ''
         for i in range(dim+1):  # <-x^0,x^1,x^2,y^1,y^2の係数
             z += w[i]*(x**i)
+            linename += f'{w[i]:.2f}x^{i}+'
         for j in range(1, dim+1):
             z += w[j+dim]*(y**j)
-        return z
+            linename += f'{w[j+dim]:.2f}y^{j}+'
+        linename = linename[:-1]
+        return z, linename
 
     return predict_z, w
 
@@ -58,14 +99,14 @@ def main():
 
     parser.add_argument('filepath', type=str, help='csv file name')
     parser.add_argument('--dim', type=int, default=1, help='x1 & x2 dimention number')
-    parser.add_argument('--lamda', type=float, default=0, help='lamda')
+    parser.add_argument('--l', type=float, default=0, help='lambda')
     args = parser.parse_args()
     filepath = args.filepath
     dim = args.dim
-    name = filepath.split('.')[0]
-    lamda = args.lamda
+    filename = filepath.split('.')[0]
+    l = args.l
 
-    with open(filepath, encoding='utf8', newline='') as f:
+    with open(filepath, encoding='utf8', newline=''):
         df = pd.read_csv(filepath).values
         x1 = df[:, :df.shape[1]-1]
         x2 = df[:, df.shape[1]-1:]
@@ -74,65 +115,53 @@ def main():
             print('df.shape[1] = 2')
 
             # 多項式曲線フィッティング
-            predict_y, w = least_squares(x1, x2, dim)
-
-            df_w = pd.DataFrame()
-            df_w = pd.concat([df_w, pd.DataFrame([w])])
+            predict_y, w = least_squares_2d(x1, x2, dim, l)
 
             x = np.linspace(min(x1), max(x1), len(x1))
-            y = predict_y(x)
+            y, linename = predict_y(x)
 
-            fig, ax = plt.subplots()           
-            line_original = ax.plot(x1, x2, '.', label='Observed')
-            line_approximation = ax.plot(x,y,color = 'orange')  # label = r'$f(x)=w[3]*x^3+w[2]*x^2+w[1]*x+w[0]$'
-            ax.set(xlabel="$x_1$", ylabel="$x_2$", title=f'{name}' + f'  dim={dim}')
-            ### 下の式をまとめて書けないか...n次式の時として
-            # dim = 1  :1次式の時
-            if dim == 1:
-                ax.legend([line_original[0],line_approximation[0]],['original data',f'{w[1]:.2f}x+{w[0]:.2f}'],loc='upper left')
-            # dim = 2  :2次式の時
-            elif dim == 2:
-                ax.legend([line_original[0],line_approximation[0]],['original data',f'y={w[2]:.2f}x^2+{w[1]:.2f}x+{w[0]:.2f}'],loc='upper left')
-            # dim = 3  :3次式の時
-            elif dim == 3:
-                ax.legend([line_original[0],line_approximation[0]],['original data',f'y={w[3]:.2f}x^3+{w[2]:.2f}x^2+{w[1]:.2f}x+{w[0]:.2f}'],loc='upper left')
+            fig, ax = plt.subplots()
+            ax.plot(x1, x2, '.', label = 'Observed')
+            ax.plot(x, y, color = 'orange', label = linename) 
+            ax.set(xlabel="$x_1$", ylabel="$x_2$", title=f'{filename}' + f'  dim={dim}')
 
+            plt.legend()
             plt.tight_layout()
-            plt.savefig(f'{name}_dim{dim}.png')
+            plt.savefig(f'{filename}_dim{dim}.png')
             plt.show()
             plt.close()
 
         elif df.shape[1] == 3:
+            print('data.shape[1] = 3')
             x3 = x2
             x1 = df[:, :1]
             x2 = df[:, 1:2]
-            print('data.shape[1] = 3')
 
-            predict_z, w = least_squares_3(x1, x2, x3, dim, lamda)
+            predict_z, w = least_squares_3d(x1, x2, x3, dim, l)
 
             x = np.linspace(min(x1), max(x1), len(x1))
             y = np.linspace(min(x2), max(x2), len(x2))
             _x, _y = np.meshgrid(x, y)
-            _z = predict_z(x, y)
+            _z, linename = predict_z(x, y)
 
-            # plot            
+            # plot     
             fig = plt.figure(figsize=(10,6))
             ax1 = fig.add_subplot(121, projection='3d')
-            ax1.plot_wireframe(_x, _y, _z, linewidth=0.3, color = 'orange', label='Predected data')
+            ax1.plot_wireframe(_x, _y, _z, linewidth=0.3, color = 'orange', label=linename)
             ax1.scatter(x1, x2, x3, label='Observed data')
-            ax1.view_init(elev=5, azim=3)  # elev: z, azim:x,y
-            ax1.set(xlabel="$x_1$", ylabel="$x_2$", zlabel="$x_3$", title=f'{name}' + f'  dim={dim}')
+            ax1.view_init(elev=10, azim=4)  # elev: z, azim:x,y
+            ax1.set(xlabel="$x_1$", ylabel="$x_2$", zlabel="$x_3$", title=f'{filename}' + f'  dim={dim}')
             ax1.legend()
             
             ax2 = fig.add_subplot(122, projection='3d')
             ax2.scatter(x1, x2, x3, label='Observed data')
-            ax2.view_init(elev=5, azim=3)
-            ax2.set(xlabel="$x_1$", ylabel="$x_2$", zlabel="$x_3$", title=f'{name}')
+            ax2.view_init(elev=10, azim=4)
+            ax2.set(xlabel="$x_1$", ylabel="$x_2$", zlabel="$x_3$", title=f'{filename}')
             ax2.legend()
             
             plt.subplots_adjust(left=0.05, right=0.9, top=0.9, wspace=0.2, hspace=0.35)
-            plt.savefig(f'{name}_dim{dim}.png')
-            plt.savefig(f'{name}_dim{dim}_turned.png')
+            # plt.savefig(f'{filename}_dim{dim}.png')
+            plt.savefig(f'{filename}_dim{dim}_turned.png')
             plt.show()
             plt.close()
 
