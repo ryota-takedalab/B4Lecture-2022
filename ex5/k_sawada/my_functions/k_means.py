73654faf6b1087c83ca3d812a8e9395160aa57d2
KateSawada <sawapy630@gmail.com>
2022-06-02 21:10:31 +0900

    finish k-means

--- ex5/k_sawada/my_functions/k_means.py
+++ ex5/k_sawada/my_functions/k_means.py
@@ -1,25 +1,37 @@
 import numpy as np
 
 
-def k_means(data, classes=3):
+def k_means(data, clusters=3):
+    """k-means clustering
+
+    Args:
+        data (ndarray, axis=(data count, each data)): input data
+        clusters (int, optional): clusters count. Defaults to 3.
+
+    Returns:
+        ndarray: ndarray of cluster index
+    """
     length = len(data)
     dimension = data.shape[1]
     
     # pick initial centroids
     # current implement is random
-    centroids = np.random.randint(0, length, size=(classes))
+    centroids = np.random.randint(0, length, size=(clusters))
     centroids = data[centroids]
 
-    distances = np.zeros((length, classes))
+    distances = np.zeros((length, clusters))
 
     while True:
-        # NOTE: 行列演算に置き換えれたら良いな
+        # NOTE: for文を行列演算に置き換えれたら良いけど, 思いつかん
+        # calculate distance between each data and centroids
         for i in range(length):
             distances[i] = np.sum(np.power(data[i] - centroids, 2), axis=1)
+        # classify based on distance
         belongs = np.argmin(distances, axis=1)
         
-        centroids_new = np.zeros((classes, dimension))
-        for c in range(classes):
+        # update centroids
+        centroids_new = np.zeros((clusters, dimension))
+        for c in range(clusters):
             centroids_new[c] = np.average(data[belongs == c], axis=0)
 
         # judge convergence
@@ -27,4 +39,22 @@ def k_means(data, classes=3):
             break
         centroids = centroids_new
     
-    return
+    return belongs
+
+
+def data_separate(data, labels, clusters):
+    """data separate based on labels
+
+    Args:
+        data (ndarray, axis=(data count, each data)): input data
+        labels (ndarray, axis=data count): clustered label
+        clusters (int): cluster count
+
+    Returns:
+        list of ndarray: separate with each clusters
+    """
+    separated = []
+    for i in range(clusters):
+        separated.append(data[labels == i])
+
+    return separated
