aedca4c49d7b4e6d6f73b0d161c9b0af67f7f1d1
ryota-takedalab <tanaka.ryota@g.sp.m.is.nagoya-u.ac.jp>
2022-06-24 15:52:53 +0900

    一通り実装

--- ex8/r_tanaka/main.py
+++ ex8/r_tanaka/main.py
@@ -3,7 +3,6 @@ import time
 from functools import wraps
 import pickle
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import confusion_matrix, accuracy_score
@@ -15,9 +14,9 @@ def stop_watch(func):
         start = time.time()
         result = func(*args, **kargs)
         process_time = round(time.time() - start, 4)
-        print("--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--")
+        print("-----------------------------------------------------------------")
         print(f"It took {process_time} sec to process {func.__name__}")
-        print("--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--")
+        print("-----------------------------------------------------------------")
         return result
     return wrapper
 
@@ -45,21 +44,21 @@ def forward_algorithm(k, PI, A, B):
     model = PI.shape[0]
     c = PI.shape[1]
     alpha = np.zeros((model, n, c))
-    
+
     # 各モデルについて計算
     for h in range(model):
         # 初期化
         alpha[h, 0, :] = PI[h, :, 0] * B[h, :, 0]  # (model, n, c)
-        print(alpha.shape)
-        
+
         # 再帰的計算
         for t in range(1, n):
-            alpha[h, t, :] = np.sum(alpha[h, t - 1, :] * A[h].T, axis = 1) * B[h, :, k[t]]
-    
+            alpha[h, t, :] = np.sum(alpha[h, t - 1, :] * A[h].T, axis=1) * B[h, :, k[t]]
+
     # 確率の計算
-    probability = np.sum(alpha, axis = 2)  # (model, n)
+    probability = np.sum(alpha, axis=2)  # (model, n)
+    # HMMの種類を予測
     predict = np.argmax(probability[:, -1])
-    
+
     return predict
 
 
@@ -84,15 +83,79 @@ def run_forward_algorithm(output, PI, A, B):
         m: 出力記号の数
     """
     predict = np.zeros(len(output))
-    
+
     for k in range(len(output)):
-        predict[k] = forward_algorithm(output[k], PI, A, B)    
-    
+        predict[k] = forward_algorithm(output[k], PI, A, B)
+
     return predict
 
 
-def viterbi_algorithm():
-    return 0
+def viterbi_algorithm(k, PI, A, B):
+    """viterbi algorithm
+
+    Args:
+        k (np.ndarray): 出力系列. shape = (n, ).
+        PI (np.ndarray): 初期確率. shape = (h, c, 1).
+        A (np.ndarray): 状態遷移確率行列. shape = (h, c, c).
+        B (np.ndarray): 出力確率. shape = (h, c, m).
+
+    Returns:
+        int: 出力系列kのモデル予測結果
+
+    Note:
+        n: 観測回数
+        h: HMMのモデル数
+        c: 状態数
+        m: 出力記号の数
+    """
+    n = len(k)
+    model = PI.shape[0]
+    c = PI.shape[1]
+    phi = np.zeros((model, n, c))
+    probability = np.zeros(model)
+
+    # 各モデルについて計算
+    for h in range(model):
+        # 初期化
+        phi[h, 0, :] = PI[h, :, 0] * B[h, :, 0]  # (model, n, c)
+        # 再帰的計算
+        for t in range(1, n):
+            phi[h, t, :] = np.max(phi[h, t - 1, :] * A[h].T, axis=1) * B[h, :, k[t]]
+        # 終了
+        probability[h] = np.max(phi[h, n - 1, :])
+
+    # HMMの種類を予測
+    predict = np.argmax(probability)
+
+    return predict
+
+
+@stop_watch
+def run_viterbi_algorithm(output, PI, A, B):
+    """run viterbi algorithm
+
+    Args:
+        output (np.ndarray): 全出力系列. shape = (k, n).
+        PI (np.ndarray): 初期確率. shape = (h, c, 1).
+        A (np.ndarray): 状態遷移確率行列. shape = (h, c, c).
+        B (np.ndarray): 出力確率. shape = (h, c, m).
+
+    Returns:
+        np.adarray: 予測結果
+
+    Note:
+        k: 出力系列数
+        n: 観測回数
+        h: HMMのモデル数
+        c: 状態数
+        m: 出力記号の数
+    """
+    predict = np.zeros(len(output))
+
+    for k in range(len(output)):
+        predict[k] = viterbi_algorithm(output[k], PI, A, B)
+
+    return predict
 
 
 def main():
@@ -120,28 +183,29 @@ def main():
     A = np.array(data["models"]["A"])
     B = np.array(data["models"]["B"])
 
-    '''
-    print("answer_models")
-    print(answer_models)
-    print(answer_models.shape)
-    print(type(answer_models))
-    print("output")
-    print(output)
-    print(output.shape)
-    print(type(output))
-    print("PI")
-    print(PI)
-    print(PI.shape)
-    print("A")
-    print(A)
-    print(A.shape)
-    print("B")
-    print(B)
-    print(B.shape)
-    '''
-    
-    predict = run_forward_algorithm(output, PI, A, B)
-    print(predict)
+    # predict HMM by forward algorithm
+    predict_fwd = run_forward_algorithm(output, PI, A, B)
+    acc_fwd = accuracy_score(answer_models, predict_fwd) * 100
+    fig_fwd = plt.figure()
+    cm_fwd = confusion_matrix(answer_models, predict_fwd)
+    sns.heatmap(cm_fwd, annot=True, cmap='Blues')
+    fig_fwd.suptitle(f"{fname} Forward Algorithm\n(Acc. {acc_fwd:.1f}%)")
+    plt.xlabel("Predicted model")
+    plt.ylabel("Actual model")
+    fig_fwd.tight_layout()
+    fig_fwd.savefig(fname + "_cm_fwd.png")
+
+    # predict HMM by viterbi algorithm
+    predict_vtb = run_viterbi_algorithm(output, PI, A, B)
+    acc_vtb = accuracy_score(answer_models, predict_vtb) * 100
+    fig_vtb = plt.figure()
+    cm_vtb = confusion_matrix(answer_models, predict_vtb)
+    sns.heatmap(cm_vtb, annot=True, cmap='Blues')
+    fig_vtb.suptitle(f"{fname} Viterbi Algorithm\n(Acc. {acc_vtb:.1f}%)")
+    plt.xlabel("Predicted model")
+    plt.ylabel("Actual model")
+    fig_vtb.tight_layout()
+    fig_vtb.savefig(fname + "cm_vtb.png")
 
 
 if __name__ == "__main__":
